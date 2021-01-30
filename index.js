d52050c7c2bd19d753a4fcb034f41de4fb50058e
coyjennings <coy@coyjenningswdp.com>
2021-01-30 08:26:39 -0700

    connect mongodb

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose_con = require('mongoose');
+mongoose_con.connect('mongodb+srv://coyjennings:Coydesign1985$@boiler-plate.jwpvc.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB connnected...'))
+  .catch(err => console.log(err))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
