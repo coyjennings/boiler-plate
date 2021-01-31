eef74c7368f3299b6eaff9196ddbe631281c86f4
coyjennings <coy@coyjenningswdp.com>
2021-01-31 07:31:42 -0700

    BodyParser & create User

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose_con = require('mongoose');
 mongoose_con.connect('mongodb+srv://coyjennings:Coydesign1985$@boiler-plate.jwpvc.mongodb.net/test?retryWrites=true&w=majority', {
@@ -11,6 +20,17 @@ mongoose_con.connect('mongodb+srv://coyjennings:Coydesign1985$@boiler-plate.jwpv
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
